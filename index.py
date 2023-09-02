cf5003ac1d14d471214ea0f6b63c8270d8f19965
Maryamshraf <142664253+Maryamshraf@users.noreply.github.com>
2023-09-02 19:30:11 +0300

    cw4

---
+++ index.py
@@ -0,0 +1,40 @@
+def my_name(name):
+    print(name)
+my_name("my name is maryam")
+def my_meal(food,drink):
+    print(food+ " "+drink)
+my_meal("i like to eat pizza" ," while  drinking cola" )
+def cube(number):
+   #number= int(input("enter a number:"))
+   return number**3
+result = cube(8)
+print(result)
+
+    
+
+
+def by_three(number):
+    #number= int(input("enter a number:"))
+    if number%3==0:
+        return cube(number)
+        
+    else:
+        return False
+print(by_three(7))
+
+
+   
+      
+
+   
+   
+   
+
+
+
+    
+    
+      
+
+
+     
